bc0c3d996eeb4c0f0e8ecf8890ddb14240feab91
Jie Wang <1293097873@qq.com>
2017-11-23 15:59:09 +0800

    框架搭建+基本设置

---
+++ WJios_takeoutapp_1.0/MainViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MainViewController.swift
+//  WJios_takeoutapp_1.0
+//
+//  Created by WangJie on 22/11/2017.
+//  Copyright © 2017 WangJie. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        addChildVC(storyname: "Home")
+//        addChildVC(storyname: "Discover")
+//        addChildVC(storyname: "Order")
+//        addChildVC(storyname: "Profile")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+////在tabbar中添加自控制器
+//    private func addChildVC(storyname: String){
+//        let childvc = UIStoryboard(name: storyname, bundle: nil).instantiateInitialViewController()!
+//        addChildViewController(childvc)
+//    }
+//
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
